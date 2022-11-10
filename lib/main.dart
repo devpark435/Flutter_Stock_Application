6e046a6b42d9a0789f18a5decd71c6d7b9d9e4e4
H2SU <hisu138@gmail.com>
2022-11-10 17:26:40 +0900

    login_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:stock_flutter_app/screens/ChartPage.dart';
 import '/screens/ListPage.dart';
 import './screens/SearchPage.dart';
+import 'login/auth_service.dart';
 import 'screens/loginPage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // main 함수에서 async 사용하기 위함
+  await Firebase.initializeApp(); // firebase 앱 시작
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -1,4 +1,8 @@
 import 'dart:ffi';
+import 'package:provider/provider.dart';
+
+import '../login/auth_service.dart';
+import '../main.dart';
 import 'ChartPage.dart';
 import 'ListPage.dart';
 import 'package:flutter/material.dart';
@@ -18,154 +22,191 @@ class _LoginPage extends State<LoginPage> {
   int searchText = 0;
   String inputID = '';
   String inputPW = '';
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Palette.bgColor,
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: 50,
-                decoration: BoxDecoration(
-                    color: Palette.containerColor,
-                    borderRadius: BorderRadius.circular(15)),
-                child: GestureDetector(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextField(
-                      onChanged: (text) {
-                        setState(() {
-                          inputID = text;
-                        });
-                      },
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'ID',
-                        hintStyle: TextStyle(
-                          color: Palette.bgColor,
+    return Consumer<AuthService>(
+      builder: (context, authService, child) {
+        return Scaffold(
+          backgroundColor: Palette.bgColor,
+          body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Palette.containerColor,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: GestureDetector(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextField(
+                          controller: emailController,
+                          onChanged: (text) {
+                            setState(() {
+                              inputID = text;
+                            });
+                          },
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'E-Mail',
+                            hintStyle: TextStyle(
+                              color: Palette.bgColor,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: 50,
-                decoration: BoxDecoration(
-                    color: Palette.containerColor,
-                    borderRadius: BorderRadius.circular(15)),
-                child: GestureDetector(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextField(
-                      onChanged: (text) {
-                        setState(() {
-                          inputPW = text;
-                        });
-                      },
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
-                        hintStyle: TextStyle(
-                          color: Palette.bgColor,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Palette.containerColor,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: GestureDetector(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextField(
+                          controller: passwordController,
+                          onChanged: (text) {
+                            setState(() {
+                              inputPW = text;
+                            });
+                          },
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Password',
+                            hintStyle: TextStyle(
+                              color: Palette.bgColor,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      height: 50,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Palette.containerColor,
-                          borderRadius: BorderRadius.circular(15)),
-                      child: GestureDetector(
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          height: 50,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: Palette.containerColor,
+                              borderRadius: BorderRadius.circular(15)),
+                          child: ElevatedButton(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {
+                              // 로그인
+                              authService.signIn(
+                                email: emailController.text,
+                                password: passwordController.text,
+                                onSuccess: () {
+                                  // 로그인 성공
+                                  /* ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("로그인 성공"),
+                                  )); */
+
+                                  // HomePage로 이동
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => MarketPage()),
+                                  );
+                                  showDialog(
+                                      context: context,
+                                      builder: ((context) {
+                                        return AlertDialog(
+                                          title: const Text('LoginData'),
+                                          content: SingleChildScrollView(
+                                            child: ListBody(
+                                              children: [
+                                                Text('로그인성공'),
+                                                Text('E-Mail : ${inputID}'),
+                                                Text('PW : ${inputPW}')
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      }));
+                                },
+                                onError: (err) {
+                                  // 에러 발생
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text(err),
+                                  ));
+                                },
+                              );
+                            },
+                          ),
                         ),
-                        onTap: () {
-                          showDialog(
-                              context: context,
-                              builder: ((context) {
-                                return AlertDialog(
-                                  title: const Text('LoginData'),
-                                  content: SingleChildScrollView(
-                                    child: ListBody(
-                                      children: [
-                                        Text('ID : ${inputID}'),
-                                        Text('PW : ${inputPW}')
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              }));
-                        },
-                      ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          height: 50,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: Palette.containerColor,
+                              borderRadius: BorderRadius.circular(15)),
+                          child: GestureDetector(
+                            child: Text(
+                              'SignUp',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          SignupPage(title: 'title')));
+                            },
+                          ),
+                        )
+                      ],
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      height: 50,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Palette.containerColor,
-                          borderRadius: BorderRadius.circular(15)),
-                      child: GestureDetector(
-                        child: Text(
-                          'SignUp',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      SignupPage(title: 'title')));
-                        },
-                      ),
-                    )
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => MyPage(title: 'title')));
-        },
-        backgroundColor: Palette.containerColor,
-      ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MyPage(title: 'title')));
+            },
+            backgroundColor: Palette.containerColor,
+          ),
+        );
+      },
     );
   }
 }
